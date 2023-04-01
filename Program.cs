56c2916be82faf99c86003735ca277ffc1fcbc2e
miguel <miguelpositivo02@gmail.com>
2023-03-31 22:44:16 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+String nome,sobrenome;
+Console.Write("digite seu nome");
+nome =Console.ReadLine()!;
+Console.Write("digite seu sobrenome");
+sobrenome =Console.ReadLine()!;
+Console.Write($"{nome} {sobrenome}");
+Console.Write($"Nome de catálogo :{sobrenome.ToUpper()},{nome}");
